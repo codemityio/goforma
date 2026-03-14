4bc1f6dde2e3c7da4b147668d594dfb5ec176294
Krzysztof Zaucha <krzysztof@zaucha.co.uk>
2026-03-14 15:09:00 +0000

    add project files

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/codemity/goforma/internal/app"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	application := app.New(
+		app.WithValues(
+			name,
+			`A tool to support work with Markdown.`,
+			version,
+			copyright,
+			authorName,
+			authorEmail,
+			buildTime,
+		),
+	)
+
+	application.Commands = []*cli.Command{}
+
+	if e := application.Run(os.Args); e != nil {
+		log.Fatalf("error occurred during execution")
+	}
+}
