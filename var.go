223b0cf197d739a2720da7c43bce6d7eb1cf02e5
Krzysztof Zaucha <krzysztofzaucha@users.noreply.github.com>
2026-03-14 15:17:27 +0000

    Add project files and update README (#1)

---
+++ var.go
@@ -0,0 +1,11 @@
+//nolint:gochecknoglobals
+package main
+
+var (
+	name        = "goforma"
+	version     = ""
+	copyright   = "codemity"
+	authorName  = "codemity"
+	authorEmail = ""
+	buildTime   = ""
+)
